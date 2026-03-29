8eb7af4a3b14b72eb5d6d0a088f842c023ea2a0f
madartz1 <114952549+madartz1@users.noreply.github.com>
2026-03-29 17:06:18 -0400

    Update Products.js

--- Products.js
+++ Products.js
@@ -12,7 +12,7 @@ window.products = [
     ],
     description:
       "A premium hoodie designed to represent patience, recovery, and personal healing. Proceeds help support community wellness initiatives.",
-    buyLink: "https://buy.stripe.com/REPLACE_WITH_STRIPE_LINK_1"
+    buyLink: "https://buy.stripe.com/aFabJ0fpq1w4d0P18c8ww01
   },
 
   {
